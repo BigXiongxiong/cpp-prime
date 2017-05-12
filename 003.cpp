e576ac4cbe0ca94a12a5bc5e978dbcd0d0f6bfb3
Dezhu Cui <cui@DezhudeMacBook-Pro.local>
2017-05-12 15:27:48 +0800

    Exercise 1.4

---
+++ 003.cpp
@@ -0,0 +1,21 @@
+
+/*
+ *      Filename: 003.cpp
+ *
+ *        Author: Dezhu Cui
+ *         Email: chinacuidezhu@gmail.com
+ *          Blog: https://blog.ijs.me/
+ *   Description: ---
+ *        Create: 2017-05-12 15:23:59
+* Last Modified: 2017-05-12 15:26:18
+ */
+
+#include <iostream>
+int main() {
+  std::cout << "Enter two numbers:" << std::endl;
+  int v1 = 0, v2 = 0;
+  std::cin >> v1 >> v2;
+  std::cout << "The multiplication of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+
+  return 0;
+}
