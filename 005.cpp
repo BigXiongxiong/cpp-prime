e3acbbe39e32946052e5fdd66ceef9c6a2aa3ff6
cuidezhu <chinacuidezhu@gmail.com>
2017-05-12 15:57:49 +0800

    Exercise 1.6

---
+++ 005.cpp
@@ -0,0 +1,23 @@
+
+/*
+ *      Filename: 005.cpp
+ *
+ *        Author: Dezhu Cui
+ *         Email: chinacuidezhu@gmail.com
+ *          Blog: https://blog.ijs.me/
+ *   Description: ---
+ *        Create: 2017-05-12 15:53:50
+ * Last Modified: 2017-05-12 15:57:17
+ */
+
+#include <iostream>
+int main() {
+  std::cout << "Enter two numbers:" << std::endl;
+  int v1 = 0, v2 = 0;
+  std::cin >> v1 >> v2;
+  std::cout << "The sum of " << v1;
+  std::cout << " and " << v2;
+  std::cout << " is " << v1 + v2 << std::endl;
+
+  return 0;
+}
