0e56f558cef1b7089f79b8e9855f25efb8ad9447
Dezhu Cui <cui@DezhudeMacBook-Pro.local>
2017-05-12 15:16:40 +0800

    Exercise 1.3

---
+++ 002.cpp
@@ -0,0 +1,18 @@
+
+/*
+ *      Filename: 002.cpp
+ *
+ *        Author: Dezhu Cui
+ *         Email: chinacuidezhu@gmail.com
+ *          Blog: https://blog.ijs.me/
+ *   Description: ---
+ *        Create: 2017-05-12 15:13:15
+* Last Modified: 2017-05-12 15:16:17
+ */
+
+#include <iostream>
+int main() {
+  std::cout << "Hello, World";
+  
+  return 0;
+}
