da4532fba3f27c0abb60f9eb2aaaba0b69e2d91d
cuidezhu <chinacuidezhu@gmail.com>
2017-05-18 01:46:03 +0800

    Exercise 1.13

---
+++ 015.cpp
@@ -0,0 +1,22 @@
+
+/*
+ *      Filename: 015.cpp
+ *
+ *        Author: Dezhu Cui
+ *         Email: chinacuidezhu@gmail.com
+ *          Blog: https://blog.ijs.me/
+ *   Description: ---
+ *        Create: 2017-05-18 01:42:44
+ * Last Modified: 2017-05-18 01:45:24
+ */
+
+#include <iostream>
+
+int main()
+{
+  for (int val = 10; val >=0; --val) {
+    std::cout << val << std::endl;
+  }
+
+  return 0;
+}
