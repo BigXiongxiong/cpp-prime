5a208fe22af96269a4baaa710e7bbd293c0e1098
cuidezhu <chinacuidezhu@gmail.com>
2017-05-18 11:30:50 +0800

    Exercise 1.16

---
+++ 018.cpp
@@ -0,0 +1,25 @@
+
+/*
+ *      Filename: 018.cpp
+ *
+ *        Author: Dezhu Cui
+ *         Email: chinacuidezhu@gmail.com
+ *          Blog: https://blog.ijs.me/
+ *   Description: ---
+ *        Create: 2017-05-18 11:27:18
+ * Last Modified: 2017-05-18 11:29:43
+ */
+
+#include <iostream>
+
+int main()
+{
+  int sum = 0, value = 0;
+
+  for (; std::cin >> value; sum += value) {
+  }
+
+  std::cout << "Sum is: " << sum << std::endl;
+
+  return 0;
+}
