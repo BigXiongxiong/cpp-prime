b2b899ff9d9959636560c13270949e440dc33fb7
cuidezhu <chinacuidezhu@gmail.com>
2017-05-20 06:32:47 +0800

    Exercise 2.8

---
+++ 025.cpp
@@ -0,0 +1,20 @@
+
+/*
+ *      Filename: 025.cpp
+ *
+ *        Author: Dezhu Cui
+ *         Email: chinacuidezhu@gmail.com
+ *          Blog: https://blog.ijs.me/
+ *   Description: ---
+ *        Create: 2017-05-20 06:27:55
+ * Last Modified: 2017-05-20 06:32:22
+ */
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "2\t\115\n";
+
+  return 0;
+}
