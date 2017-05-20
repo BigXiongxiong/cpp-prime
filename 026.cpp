2587e1735bac237de592e9528e726fda4fe6caa5
cuidezhu <chinacuidezhu@gmail.com>
2017-05-20 11:42:29 +0800

    Exercise 2.9

---
+++ 026.cpp
@@ -0,0 +1,27 @@
+
+/*
+ *      Filename: 026.cpp
+ *
+ *        Author: Dezhu Cui
+ *         Email: chinacuidezhu@gmail.com
+ *          Blog: https://blog.ijs.me/
+ *   Description: ---
+ *        Create: 2017-05-20 11:33:58
+ * Last Modified: 2017-05-20 11:42:10
+ */
+
+#include <iostream>
+
+int main()
+{
+  int input_value;
+  std::cin >> input_value;
+  
+  int i(3.14);
+
+  double salary = wage = 9999.99;
+
+  int i1 = 3.14;    // warning
+
+  return 0;
+}
