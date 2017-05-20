9d828b7c34fbd9b79d30847df1add09d3cddf1cf
cuidezhu <chinacuidezhu@gmail.com>
2017-05-20 18:24:34 +0800

    Exercise 2.13

---
+++ 029.cpp
@@ -0,0 +1,23 @@
+
+/*
+ *      Filename: 029.cpp
+ *
+ *        Author: Dezhu Cui
+ *         Email: chinacuidezhu@gmail.com
+ *          Blog: https://blog.ijs.me/
+ *   Description: ---
+ *        Create: 2017-05-20 18:22:54
+ * Last Modified: 2017-05-20 18:24:10
+ */
+
+#include <iostream>
+
+int i = 42;
+
+int main()
+{
+  int i = 100;
+  int j = i;
+
+  std::cout << j << std::endl;
+}
